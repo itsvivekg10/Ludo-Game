99a16c626d497dbf96eba98a30d26d09a324b04a
vivek gupta <itsvivekg10@gmail.com>
2025-10-28 11:26:40 +0530

    blank files

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
+import GamePage from "./components/GamePage"
+import LandingPage from "./components/LandingPage"
 
 
 function App() {
 
   return (
     <>
-      <h1>Ludo Game</h1>
+    
+ 
     </>
   )
 }
 
 export default App
-// // Integrates game logic with board and dice.
-// //  Uses state from Member 3
-// //  and functions from GameLogic.js.
-// Imports Board, Dice, and PlayerInfo.
-// Holds state: currentPlayer, diceValue, tokens, winner.
