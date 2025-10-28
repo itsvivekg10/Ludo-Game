36a05b1411ac5124d717082ddd8b0791722b2de9
Anjana E S <anjana@example.com>
2025-10-28 11:31:02 +0530

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import GamePage from "./components/GamePage"
-import LandingPage from "./components/LandingPage"
-
+import React from "react";
+import Board from "./components/Board";
+import GameLogicProvider from "./components/GameLogic";
+import PlayerInfo from "./components/PlayerInfo";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-    
- 
-    </>
-  )
+    <GameLogicProvider>
+      <div className="App">
+        <div style={{ minWidth: 450 }}>
+          <Board />
+        </div>
+        <div style={{ width: 320 }}>
+          <PlayerInfo />
+        </div>
+      </div>
+    </GameLogicProvider>
+  );
 }
 
-export default App
+export default App;
